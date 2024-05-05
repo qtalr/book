5e582539f1f82a72f8d1f4320ada7fce04364af3
Jerid Francom <francojc@wfu.edu>
2024-05-05 12:39:10 -0400

    removes About from PDF

--- ignore_index.lua
+++ ignore_index.lua
@@ -1,3 +1,4 @@
+-- @e-miz https://github.com/quarto-dev/quarto-cli/issues/2140#issuecomment-2000383217
 function Header(element)
   if (element.classes:includes("content-hidden") and quarto.doc.isFormat(element.attributes["when-format"]))
       or (element.classes:includes("content-visible") and not quarto.doc.isFormat(element.attributes["when-format"])) then
